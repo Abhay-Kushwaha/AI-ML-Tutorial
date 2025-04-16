38930316ce9f54695c794bb70c669343c7bdc003
Abhay-Kushwaha <abhay369kumar@gmail.com>
2025-04-16 15:48:54 +0530

    updated

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -6,6 +6,11 @@ from sklearn.model_selection import train_test_split
 
 from sklearn.metrics import (confusion_matrix, classification_report, roc_auc_score, roc_curve, ConfusionMatrixDisplay)
 
-# Create a sample dataset
 X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_redundant=2, n_classes=2, random_state=42)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+model=LogisticRegression()
+model.fit(X_train,y_train)
+y_pred= model.predict(X_test)
+print(y_pred)
